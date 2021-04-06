18ac1488760a46e94fcf794de5b133a84d2762d2
Geoff Hing <geoffhing@gmail.com>
2021-04-05 22:50:47 -0500

    Load Table B03002 into a SQL database

--- etl/race.mk
+++ etl/race.mk
@@ -1,13 +1,16 @@
 # ACS 5-year
 
-# Table B03002:  Hispanic or Latino Origin by Race, zctas
+# Table B03002: Hispanic or Latino Origin by Race, zctas
 $(DATA_DIR_PROCESSED)/acs5_2019_race_zctas.csv: | $(DATA_DIR_PROCESSED)
-	censusdatadownloader --data-dir $(DATA_DIR) --year 2019 race zctas
+	pipenv run censusdatadownloader --data-dir $(DATA_DIR) --year 2019 race zctas
 
 # Table B03002: Hispanic or Latino Origin by Race, tracts
+$(DATA_DIR_PROCESSED)0/acs5_2019_race_tracts.csv: | $(DATA_DIR_PROCESSED)
+	pipenv run censusdatadownloader --data-dir $(DATA_DIR) --year 2019 race tracts
+
 $(DATA_DIR_PROCESSED)/acs5_2018_race_tracts.csv: | $(DATA_DIR_PROCESSED)
-	censusdatadownloader --data-dir $(DATA_DIR) --year 2018 race tracts
+	pipenv run censusdatadownloader --data-dir $(DATA_DIR) --year 2018 race tracts
 
 # Table B03002: Hispanic or Latino Origin by Race, block groups 
 $(DATA_DIR_PROCESSED)/acs5_2018_race_blockgroups.csv: | $(DATA_DIR_PROCESSED)
-	downloadacs5bg --data-dir $(DATA_DIR) --year 2018 race
+	pipenv run downloadacs5bg --data-dir $(DATA_DIR) --year 2018 race
--- etl/sql.mk
+++ etl/sql.mk
@@ -16,6 +16,110 @@ TABLE_PROXY_DIR := $(DATA_DIR_PROCESSED)/db_tables
 
 .PHONY: clean_table_proxy clean_sql_db
 
+# Load estimates
+
+# Table B03002: Hispanic or Latino Origin by Race, tracts
+$(TABLE_PROXY_DIR)/acs5_2019_race_tracts: $(DATA_DIR_PROCESSED)/acs5_2019_race_tracts.csv | $(TABLE_PROXY_DIR)
+	pipenv run sqlite-utils insert --csv --pk geoid $(SQLITE_DB_PATH) $(basename $(notdir $<)) $< \
+	&& pipenv run sqlite-utils transform $(SQLITE_DB_PATH) $(basename $(notdir $<)) \
+	  --type geoid text \
+	  --type name text \
+	  --type universe float \
+	  --type universe_annotation text \
+	  --type universe_moe float \
+	  --type universe_moe_annotation text \
+	  --type white_alone float \
+	  --type white_alone_annotation text \
+	  --type white_alone_moe float \
+	  --type white_alone_moe_annotation text \
+	  --type black_alone float \
+	  --type black_alone_annotation text \
+	  --type black_alone_moe float \
+	  --type black_alone_moe_annotation text \
+	  --type american_indian_and_alaska_native float \
+	  --type american_indian_and_alaska_native_annotation text \
+	  --type american_indian_and_alaska_native_moe float \
+	  --type american_indian_and_alaska_native_moe_annotation text \
+	  --type asian_alone float \
+	  --type asian_alone_annotation text \
+	  --type asian_alone_moe float \
+	  --type asian_alone_moe_annotation text \
+	  --type native_hawaiian_and_pacific_islander float \
+	  --type native_hawaiian_and_pacific_islander_annotation text \
+	  --type native_hawaiian_and_pacific_islander_moe float \
+	  --type native_hawaiian_and_pacific_islander_moe_annotation text \
+	  --type other_alone float \
+	  --type other_alone_annotation text \
+	  --type other_alone_moe float \
+	  --type other_alone_moe_annotation text \
+	  --type two_or_more_races float \
+	  --type two_or_more_races_annotation text \
+	  --type two_or_more_races_moe float \
+	  --type two_or_more_races_moe_annotation text \
+	  --type latino_alone float \
+	  --type latino_alone_annotation text \
+	  --type latino_alone_moe float \
+	  --type latino_alone_moe_annotation text \
+	  --type state text \
+	  --type county text \
+	  --type tract text \
+	  --type asians_all float \
+	  --type asians_all_moe float \
+	  --type other_all float \
+	  --type other_all_moe float \
+	&& sqlite3 -csv $(SQLITE_DB_PATH) "SELECT COUNT(*) FROM $(basename $(notdir $<));" > $@
+
+# Table B03002: Hispanic or Latino Origin by Race, zctas
+$(TABLE_PROXY_DIR)/acs5_2019_race_zctas: $(DATA_DIR_PROCESSED)/acs5_2019_race_zctas.csv | $(TABLE_PROXY_DIR)
+	pipenv run sqlite-utils insert --csv --pk geoid $(SQLITE_DB_PATH) $(basename $(notdir $<)) $< \
+	&& pipenv run sqlite-utils transform $(SQLITE_DB_PATH) $(basename $(notdir $<)) \
+	  --type geoid text \
+	  --type name text \
+	  --type universe float \
+	  --type universe_annotation text \
+	  --type universe_moe float \
+	  --type universe_moe_annotation text \
+	  --type white_alone float \
+	  --type white_alone_annotation text \
+	  --type white_alone_moe float \
+	  --type white_alone_moe_annotation text \
+	  --type black_alone float \
+	  --type black_alone_annotation text \
+	  --type black_alone_moe float \
+	  --type black_alone_moe_annotation text \
+	  --type american_indian_and_alaska_native float \
+	  --type american_indian_and_alaska_native_annotation text \
+	  --type american_indian_and_alaska_native_moe float \
+	  --type american_indian_and_alaska_native_moe_annotation text \
+	  --type asian_alone float \
+	  --type asian_alone_annotation text \
+	  --type asian_alone_moe float \
+	  --type asian_alone_moe_annotation text \
+	  --type native_hawaiian_and_pacific_islander float \
+	  --type native_hawaiian_and_pacific_islander_annotation text \
+	  --type native_hawaiian_and_pacific_islander_moe float \
+	  --type native_hawaiian_and_pacific_islander_moe_annotation text \
+	  --type other_alone float \
+	  --type other_alone_annotation text \
+	  --type other_alone_moe float \
+	  --type other_alone_moe_annotation text \
+	  --type two_or_more_races float \
+	  --type two_or_more_races_annotation text \
+	  --type two_or_more_races_moe float \
+	  --type two_or_more_races_moe_annotation text \
+	  --type latino_alone float \
+	  --type latino_alone_annotation text \
+	  --type latino_alone_moe float \
+	  --type latino_alone_moe_annotation text \
+	  --type asians_all float \
+	  --type asians_all_moe float \
+	  --type other_all float \
+	  --type other_all_moe float \
+	&& sqlite3 -csv $(SQLITE_DB_PATH) "SELECT COUNT(*) FROM $(basename $(notdir $<));" > $@
+
+
+# Load boundaries
+
 # ogr2ogr command explanation
 #
 # Many of these rules use the `ogr2ogr` command to load data from GeoJSON
@@ -42,7 +146,7 @@ TABLE_PROXY_DIR := $(DATA_DIR_PROCESSED)/db_tables
 #   database is not re-created each time. I'm not sure if `-append`
 #   is needed, and may cause problems if the same GeoJSON file
 #   is loaded multiple times without first dropping the table.
-#
+
 # Load metropolitan/micropolitan statistical area (CBSA) boundaries:
 $(TABLE_PROXY_DIR)/cbsas_2020: $(DATA_DIR_PROCESSED)/cbsas_2020.geojson | $(TABLE_PROXY_DIR)
 	ogr2ogr -f SQLite -a_srs "EPSG:4269" -nlt MULTIPOLYGON -dsco SPATIALITE=yes -nln $(basename $(notdir $<)) $(SQLITE_DB_PATH) -update -append $< \
