d112675ce93bb8224da89de33cc9fb8155b9cd37
Geoff Hing <geoff.hing@gannett.com>
2021-05-18 20:47:12 -0500

    Set types on self-response rate tables in SQL database
    Also add make rule for 2010 self-response rate data

--- etl/sql.mk
+++ etl/sql.mk
@@ -121,6 +121,33 @@ $(TABLE_PROXY_DIR)/acs5_2019_race_zctas: $(DATA_DIR_PROCESSED)/acs5_2019_race_zc
 
 $(TABLE_PROXY_DIR)/responserate_2020_responserate_tracts: $(DATA_DIR_PROCESSED)/responserate_2020_responserate_tracts.csv | $(TABLE_PROXY_DIR)
 	pipenv run sqlite-utils insert --csv --pk geoid $(SQLITE_DB_PATH) $(basename $(notdir $<)) $< \
+	&& pipenv run sqlite-utils transform $(SQLITE_DB_PATH) $(basename $(notdir $<)) \
+	  --type avg_cumulative float \
+	  --type avg_cumulative_internet float \
+	  --type max_cumulative_internet float \
+	  --type med_cumulative_internet float \
+	  --type min_cumulative_internet float \
+	  --type max_cumulative float \
+	  --type med_cumulative float \
+	  --type min_cumulative float \
+	  --type cumulative float \
+	  --type internet float \
+	  --type avg_daily float \
+	  --type avg_daily_internet float \
+	  --type max_daily_internet float \
+	  --type med_daily_internet float \
+	  --type min_daily_internet float \
+	  --type max_daily float \
+	  --type med_daily float \
+	  --type min_daily float \
+	  --type daily float \
+	  --type daily_internet float \
+	&& sqlite3 -csv $(SQLITE_DB_PATH) "SELECT COUNT(*) FROM $(basename $(notdir $<));" > $@
+
+$(TABLE_PROXY_DIR)/responserate_2010_responserate_tracts: $(DATA_DIR_PROCESSED)/responserate_2010_responserate_tracts.csv | $(TABLE_PROXY_DIR)
+	pipenv run sqlite-utils insert --csv --pk geoid $(SQLITE_DB_PATH) $(basename $(notdir $<)) $< \
+	&& pipenv run sqlite-utils transform $(SQLITE_DB_PATH) $(basename $(notdir $<)) \
+	  --type cumulative float \
 	&& sqlite3 -csv $(SQLITE_DB_PATH) "SELECT COUNT(*) FROM $(basename $(notdir $<));" > $@
 
 # Load boundaries
