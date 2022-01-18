aa6b07b562490d17e08c1a050856fbd01be5071f
Geoff Hing <geoff.hing@gannett.com>
2022-01-18 14:13:52 -0700

    Clarify geography
    I forgot to add this to the previous commit.

--- etl/language.mk
+++ etl/language.mk
@@ -1,5 +1,5 @@
 # ACS 5-year
 
-# Table C16001: Language Spoken at Home 
+# Table C16001: Language Spoken at Home, tracts
 $(DATA_DIR_PROCESSED)/acs5_2019_languageshortform_tracts.csv: | $(DATA_DIR_PROCESSED)
 	pipenv run mycensusdatadownloader --data-dir $(DATA_DIR) --year 2019 languageshortform tracts
