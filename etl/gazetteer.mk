cea8bae537412a2ad1294e70431a3badd36c6e92
Taj Morton <tajmorton@gmail.com>
2022-08-13 10:21:08 -0700

    Use `eval` to clean up and clarify rules.

--- etl/gazetteer.mk
+++ etl/gazetteer.mk
@@ -3,32 +3,29 @@
 GAZETTEER_AREA_TYPES = counties_national cosubs_national place_national
 GAZETTEER_YEARS = 2019 2020 2021
 
-# Defines list of Gazetteer Files that are available for the defined
-# area types and years. Files in this list have the directory
-# prefix "$(YEAR)_Gazetteer/", which is how the files are stored
-# on census.gov.
-# This pattern is carried forward when storing them in the raw data
-# directory because there is no trivial way to flatten the file structure
-# on disk (in $(DATA_DIR_SRC)), but maintain it in the wget call.
-GAZETTEER_FILES_ZIPS = $(foreach YEAR, $(GAZETTEER_YEARS),  \
-					    $(foreach TYPE, $(GAZETTEER_AREA_TYPES), \
-    					$(YEAR)_Gazetteer/$(YEAR)_Gaz_$(TYPE).zip \
-	    				) \
-		    		   )
-GAZETTEER_FILES_TXT = $(GAZETTEER_FILES_ZIPS:.zip=.txt)
-
+define GAZETTEER_DATASET_RULES 
+# GAZETTEER_DATASET_RULES - Targets for downloading and extracting raw
+# gazetteer datasets.
+# Parameters:
+#   - $1 = Year
+#   - $2 = Area Type
+#
 # Files are touched after unzipping to freshen their dates, so that they are newer than the source .zip file.
 # `unzip` sets the file timestamp to match the time within the zip file. This would result in the `unzip` recipe
 # running every time (due to its .zip dependency being out of date).
-$(addprefix $(DATA_DIR_SRC)/,$(GAZETTEER_FILES_TXT)): %.txt: %.zip
-	unzip -o $< $(notdir $@) -d $(dir $@)
-	touch --no-create $@
+$(DATA_DIR_SRC)/$(1)_Gaz_$(2).txt: $(DATA_DIR_SRC)/$(1)_Gaz_$(2).zip
+	unzip -o $$< $$(notdir $$@) -d $$(dir $$@)
+	touch --no-create $$@
 
-# Because raw Gazetteer files are stored in a hierarchy that matches
-# the URL on census.gov, a `mkdir` is performed to ensure the destination
-# sub-directory exists.
-# The $(patubst) call removes the $(DATA_DIR_SRC) prefix from the target name.
-$(addprefix $(DATA_DIR_SRC)/,$(GAZETTEER_FILES_ZIPS)): | $(DATA_DIR_SRC)
-	mkdir -p "$(dir $@)"
-	wget -O $@ https://www2.census.gov/geo/docs/maps-data/data/gazetteer/$(patsubst $(DATA_DIR_SRC)/%,%,$@)
+$(DATA_DIR_SRC)/$(1)_Gaz_$(2).zip: | $(DATA_DIR_SRC)
+	wget -O $$(@) https://www2.census.gov/geo/docs/maps-data/data/gazetteer/$1_Gazetteer/$1_Gaz_$2.zip
+endef
 
+# Create .txt and .zip rules for the Gazetteer Files that are available
+# in the defined area types and years. This creates rules in the form
+# of data/raw/2021_Gaz_place_national.txt.
+$(foreach YEAR, $(GAZETTEER_YEARS),  \
+  $(foreach TYPE, $(GAZETTEER_AREA_TYPES), \
+  $(eval $(call GAZETTEER_DATASET_RULES,$(YEAR),$(TYPE))) \
+  ) \
+)
--- etl/gazetteer_sql.mk
+++ etl/gazetteer_sql.mk
@@ -4,14 +4,24 @@
 # ("2019_"), so that the table name begins with a letter, so that the table name
 # does not need to be quoted when used in a SQL query.
 
-$(TABLE_PROXY_DIR)/gaz_2019_place_national: $(DATA_DIR_SRC)/2019_Gazetteer/2019_Gaz_place_national.txt | $(TABLE_PROXY_DIR)
+GAZETTEER_AREA_TYPES = counties_national cosubs_national place_national
+GAZETTEER_YEARS = 2019 2020 2021
+
+define GAZETTEER_LOAD_DATA_RULE
+# GAZETTEER_LOAD_DATA_RULE - Load raw gazetteer dataset into database.
+# Parameters:
+#   - $1 = Year
+#   - $2 = Area Type
+$(TABLE_PROXY_DIR)/gaz_$1_$2: $(DATA_DIR_SRC)/$1_Gaz_$2.txt | $(TABLE_PROXY_DIR)
+	@echo "$$@"
+	@echo "$$^"
 	@# Gazetteer files have trailing whitespace at the end of each line
 	@# which results in the colum name 'INTPTLONG                  '.
 	pipenv run bash -c ' \
-	    ./etl/trim_trailing_tsv_whitespace.py $< | \
+	    ./etl/trim_trailing_tsv_whitespace.py $$< | \
 	    sqlite-utils insert --tsv \
-	        --pk GEOID $(SQLITE_DB_PATH) $(basename $(notdir $@)) -' \
-	&& pipenv run sqlite-utils transform $(SQLITE_DB_PATH) $(basename $(notdir $@)) \
+	        --pk GEOID $(SQLITE_DB_PATH) $$(basename $$(notdir $$@)) -' \
+	&& pipenv run sqlite-utils transform $(SQLITE_DB_PATH) $$(basename $$(notdir $$@)) \
 	  --type USPS text \
 	  --type GEOID text \
 	  --type ANSICODE text \
@@ -24,4 +34,11 @@ $(TABLE_PROXY_DIR)/gaz_2019_place_national: $(DATA_DIR_SRC)/2019_Gazetteer/2019_
 	  --type AWATER_SQMI float \
 	  --type INTPTLAT float \
 	  --type INTPTLONG float \
-	&& sqlite3 -csv $(SQLITE_DB_PATH) "SELECT COUNT(*) FROM $(basename $(notdir $@));" > $@
+	&& sqlite3 -csv $(SQLITE_DB_PATH) "SELECT COUNT(*) FROM $$(basename $$(notdir $$@));" > $$@
+endef
+
+$(foreach YEAR, $(GAZETTEER_YEARS),  \
+  $(foreach TYPE, $(GAZETTEER_AREA_TYPES), \
+  $(eval $(call GAZETTEER_LOAD_DATA_RULE,$(YEAR),$(TYPE))) \
+  ) \
+)
